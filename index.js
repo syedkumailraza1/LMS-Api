ee7c0974601834b1edb435ce961749b0107d9434
Syed Kumail Raza <raza@kali.raza>
2024-08-11 20:51:43 +0530

    commit for deployment

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-import req from "express/lib/request.js";
 import { connectDB } from "./src/DB/index.js";
 import app from "./src/app.js";
-const port = process.env.PORT
 
-connectDB().then(()=>{
-    app.get((req,res)=>{
-        res.send("Hello world")
-    })
-    app.listen(port,()=>{
-        console.log(`The server is live on http//localhost:${port}`); 
-    })
-}).catch((error)=>{
+const port = process.env.PORT || 3000;
+
+connectDB().then(() => {
+    app.get('/', (req, res) => {
+        res.send("Hello world");
+    });
+    app.listen(port, () => {
+        console.log(`The server is live on http://localhost:${port}`);
+    });
+}).catch((error) => {
     console.log(`Error in db Connection: ${error}`);
-    
-})
+});
